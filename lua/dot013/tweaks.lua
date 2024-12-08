fdcc02955db4ee2675d46b539c9ab677f836eed3
Gustavo L de Mello (Guz) <contact@guz.one>
2024-12-08 17:46:41 -0300

    feat(tweaks): tweaks to existing neovim functionality and keymaps

--- lua/dot013/tweaks.lua
+++ lua/dot013/tweaks.lua
@@ -7,3 +7,20 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- Move when highlighted
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Make cursor stay in place when using J
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Just to be sure
+vim.keymap.set("n", "<C-c>", "<Esc>")
+-- Don't press Q
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { desc = "Nop", silent = true })
+
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
