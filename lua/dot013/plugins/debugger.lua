bacb4a4c21585454c65609079606e1885fdea0d0
Gustavo L de Mello (Guz) <contact@guz.one>
2025-01-28 16:57:40 -0300

    fix: debugger not being properly initiated

--- lua/dot013/plugins/debugger.lua
+++ lua/dot013/plugins/debugger.lua
@@ -2,15 +2,32 @@ return {
 	-- DAP Debugger support
 	{
 		"nvim-dap",
+		dap_of = {
+			"nvim-dap-ui",
+			"nvim-dap-virtual-text",
+			"nvim-dap-go",
+		},
 		keys = {
 			{ "<leader>b", ":lua require('dap').toggle_breakpoint()<cr>", "[Debugger] Toggle breakpoint" },
 			{ "<leader>x", ":lua require('dap').continue()<cr>", "[Debugger] Continue debugger" },
 			{ "<leader>X", ":lua require('dap').terminate()<cr>", "[Debugger] Terminate debugger" },
 			{ "<leader>C", ":lua require('dap').clear_breakpoints()<cr>", "[Debugger] Clear all breakpoints" },
+			-- UI
+			{ "<leader>xu", ":lua require('dapui').toggle()<cr>", "[Debugger] Toggle debugger UI" },
+			{
+				"<leader>K",
+				":lua require('dapui').eval(nil, { enter = true })<cr>",
+				"[Debugger] Eval var under cursor",
+			},
 		},
 		after = function()
 			local dap = require("dap")
+
 			local dapui = require("dapui")
+			dapui.setup()
+
+			local dapvt = require("nvim-dap-virtual-text")
+			dapvt.setup()
 
 			dap.listeners.before.attach.dapui_config = function()
 				dapui.open()
@@ -24,39 +41,32 @@ return {
 			dap.listeners.before.event_exited.dapui_config = function()
 				dapui.close()
 			end
+
+			-- Languages
+			local dapgo = require("dap-go")
+			dapgo.setup()
 		end,
 	},
 
 	-- Debugger UI
 	{
 		"nvim-dap-ui",
-		dap_of = { "nvim-dap" },
-		keys = {
-			{ "<leader>xu", ":lua require('dapui').toggle()<cr>", "[Debugger] Toggle debugger UI" },
-			{
-				"<leader>K",
-				":lua require('dapui').eval(nil, { enter = true })<cr>",
-				"[Debugger] Eval var under cursor",
-			},
-		},
+		dep_of = { "nvim-dap" },
+	},
+	{
+		"nvim-nio",
+		dep_of = { "nvim-dap-ui" },
 	},
 
 	-- Debugger state hover
 	{
 		"nvim-dap-virtual-text",
 		dep_of = { "nvim-dap" },
-		after = function()
-			require("nvim-dap-virtual-text").setup()
-		end,
 	},
 
 	-- Language specific debuggers
 	{
 		"nvim-dap-go",
 		dep_of = { "nvim-dap" },
-		enabled = vim.fn.executable("delve") == 1,
-		after = function()
-			require("dap-go").setup()
-		end,
 	},
 }
