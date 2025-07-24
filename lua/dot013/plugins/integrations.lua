1e8baa29866a487951dbd36f813eae4b1eedd730
Gustavo "Guz" L de Mello <contact@guz.one>
2025-07-24 07:09:37 -0300

    feat(integrations): use local activitywatch server

--- lua/dot013/plugins/integrations.lua
+++ lua/dot013/plugins/integrations.lua
@@ -2,12 +2,7 @@ return {
 	{
 		"aw-watcher.nvim",
 		after = function()
-			require("aw_watcher").setup({
-				aw_server = {
-					host = "100.69.8.56",
-					port = 5600,
-				}
-			})
+			require("aw_watcher").setup({})
 		end
 	}
 }
