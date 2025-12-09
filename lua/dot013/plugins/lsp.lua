21646b69b38e2b97ce5b38ba469cfc1e3c0b62ca
Gustavo "Guz" L de Mello <contact@guz.one>
2025-12-08 21:36:09 -0300

    feat(lsp): setup lua_ls for neovim

--- lua/dot013/plugins/lsp.lua
+++ lua/dot013/plugins/lsp.lua
@@ -12,7 +12,10 @@ local lsps = {
 		on_init = function(client)
 			if client.workspace_folders then
 				local path = client.workspace_folders[1].name
-				if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+				if
+					path ~= vim.fn.stdpath("config") and vim.loop.fs_stat(path .. "/.luarc.json")
+					or vim.loop.fs_stat(path .. "/.luarc.jsonc")
+				then
 					return
 				end
 			end
@@ -20,6 +23,10 @@ local lsps = {
 			client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
 				runtime = {
 					version = "LuaJIT",
+					path = {
+						"lua/?.lua",
+						"lua/?/init.lua",
+					},
 				},
 				workspace = {
 					checkThirdParty = false,
@@ -31,7 +38,8 @@ local lsps = {
 		end,
 		settings = {
 			Lua = {
-				workspace = { checkThirdParty = false },
+				codeLens = { enable = true },
+				hint = { enable = true, semicolin = "Disable" },
 				telemetry = { enable = false },
 			},
 		},
