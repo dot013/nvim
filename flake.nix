1c1cd0ca523dee7dfcf5baba913ab1e1a1ad5896
Gustavo "Guz" L de Mello <contact@guz.one>
2025-12-14 20:52:18 -0300

    feat(nix): provide home-manager and nixos modules directly in flake.nix

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
             inherit (pkgs) lib stdenv;
           }
       );
+    neovim-pkgs = system: import nixpkgs {inherit system;};
   in {
     formatter = forAllSystems ({pkgs, ...}: pkgs.alejandra);
 
@@ -39,7 +40,7 @@
         ...
       }: {
         neovim = pkgs.callPackage ./package.nix {
-          mdfmt = self.packages.${stdenv.hostPlatform.system}.mdfmt;
+          mdfmt = self.packages.${pkgs.system}.mdfmt;
         };
           godot-neovim = pkgs.writeShellApplication {
             name = "godot-neovim";
@@ -49,7 +50,8 @@
           src = inputs.mdfmt;
           vendorHash = "sha256-JtYvDgjUoEc1Mp7Eq8lbu9jWI+RR9yBo4ujGY+J70J4=";
         };
-        default = self.packages."${stdenv.hostPlatform.system}".neovim;
+
+        default = self.packages."${pkgs.system}".neovim;
       }
     );
     devShells = forAllSystems (
@@ -63,11 +65,101 @@
     );
 
     nixosModules = {
-      neovim = import ./nixos.nix {inherit self;};
+      neovim = {
+        config,
+        lib,
+        pkgs,
+        stdenv,
+        ...
+      }: let
+        cfg = config.neovim;
+        npkgs = neovim-pkgs pkgs.system;
+      in
+        with lib; {
+          options.neovim = {
+            enable = mkOption {
+              type = with types; bool;
+              default = true;
+            };
+            package = mkOption {
+              type = with types; package;
+              default = self.packages.${pkgs.system}.default;
+              readOnly = true;
+            };
+            defaultEditor = mkOption {
+              type = with types; bool;
+              default = true;
+            };
+          };
+          config = mkIf cfg.enable {
+            environment.variables = {
+              EDITOR = "nvim";
+            };
+            environment.systemPackages =
+              [
+                (npkgs.callPackage ./package.nix {
+                    mdfmt = self.packages.${pkgs.system}.mdfmt;
+                  }
+                  // (optionalAttrs config.programs.yazi.enable {
+                    yazi = config.programs.yazi.package;
+                  }))
+              ]
+            # Disable NixOS's Neovim
+            programs.neovim.enable = mkForce false;
+          };
+        };
+
       default = self.nixosModules.neovim;
     };
     homeManagerModules = {
-      neovim = import ./home.nix {inherit self;};
+      neovim = {
+        config,
+        lib,
+        pkgs,
+        stdenv,
+        ...
+      }: let
+        cfg = config.neovim;
+        npkgs = neovim-pkgs pkgs.system;
+      in
+        with lib; {
+          options.neovim = {
+            enable = mkOption {
+              type = with types; bool;
+              default = true;
+            };
+            vimdiffAlias = mkOption {
+              type = with types; bool;
+              default = true;
+            };
+            defaultEditor = mkOption {
+              type = with types; bool;
+              default = true;
+            };
+          };
+          config = mkIf cfg.enable {
+            home.sessionVariables = {
+              EDITOR = "nvim";
+            };
+
+            home.packages =
+              [
+                (npkgs.callPackage ./package.nix {
+                    mdfmt = self.packages.${pkgs.system}.mdfmt;
+                  }
+                  // (optionalAttrs config.programs.yazi.enable {
+                    yazi = config.programs.yazi.package;
+                  }))
+              ]
+            programs.bash.shellAliases = mkIf cfg.vimdiffAlias {vimdiff = "nvim -d";};
+            programs.fish.shellAliases = mkIf cfg.vimdiffAlias {vimdiff = "nvim -d";};
+            programs.zsh.shellAliases = mkIf cfg.vimdiffAlias {vimdiff = "nvim -d";};
+
+            # Disable home-manager's Neovim
+            programs.neovim.enable = mkForce false;
+          };
+        };
+
       default = self.homeManagerModules.neovim;
     };
   };
