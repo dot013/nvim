c18010b4f89e6f1cac27325d5d513a5532244eb1
Gustavo "Guz" L de Mello <contact@guz.one>
2026-09-11 19:58:59 -0300

    feat: improve configuration for snippets

--- lua/dot/plugins.lua
+++ lua/dot/plugins.lua
@@ -89,6 +89,34 @@ lze.load({
 							module = "lazydev.integrations.blink",
 							score_offset = 100,
 						},
+						snippets = {
+							opts = {
+								friendly_snippets = true, -- default
+
+								-- see the list of frameworks in: https://github.com/rafamadriz/friendly-snippets/tree/main/snippets/frameworks
+								-- and search for possible languages in: https://github.com/rafamadriz/friendly-snippets/blob/main/package.json
+								-- the following is just an example, you should only enable the frameworks that you use
+								extended_filetypes = {
+									c = { "cdoc" },
+									cpp = { "ccpdoc", "unreal" },
+									cs = { "csharpdoc" },
+									gdscript = { "godot" },
+									java = { "javadoc" },
+									javascript = { "jsdoc" },
+									javascriptreact = { "jsdoc" },
+									kotlin = { "kotlindoc" },
+									lua = { "luadoc" },
+									markdown = { "jekyll", "latex" },
+									php = { "phpdoc" },
+									python = { "pydoc" },
+									ruby = { "rdoc" },
+									rust = { "rustdoc" },
+									sh = { "shelldoc" },
+									typescript = { "jsdoc" },
+									typescriptreact = { "jsdoc" },
+								},
+							},
+						},
 					},
 				},
 			})
